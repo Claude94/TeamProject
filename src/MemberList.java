74e5f174357863934b342dddf93b401ab8bdc96c
Irus0-0 <ikaros9313@gmail.com>
2021-03-19 23:29:08 +0900

    정시윤 - 로그인기능완성 / 관리자 전용 기능 만드는중 -참고및 세이브용

--- src/MemberList.java
+++ src/MemberList.java
@@ -1,4 +1,3 @@
-package MiniProject;
 
 import java.util.*;
 
@@ -15,22 +14,22 @@ public class MemberList {
         };
         System.out.println("=== 동원 계시판 로그인! ===");
         System.out.print("=== 아이디: ");
-        String id = sc.next();
+        String id = sc.next();  //id 받기
         System.out.print("== 비밀번호: ");
-        String password = sc.next();
-        boolean idCheck = false;
-        boolean pwCheck = false;
-        int memberNumber = 0;
-        for (int i = 1; i < memberList.length; i++) {
-            for (int j = i; j < memberList.length; j++) {
-                if (id.equals(memberList[i][1])) {
-                    idCheck = true;
-                    memberNumber = Integer.parseInt(memberList[i][0]);
-                    break;
-                }
+        String password = sc.next(); //비밀번호 받기
+        boolean idCheck = false; // 아이디 유무
+        boolean pwCheck = false; // 비번 유무
+        int memberNumber = 0; // 0은 null로 취급   멤버 아이디를 가져오기 위함
+
+        for (int i = 1; i < memberList.length; i++) { //멤버 전체를 검토하여 id확인
+            if (id.equals(memberList[i][1])) {
+                idCheck = true;
+                memberNumber = Integer.parseInt(memberList[i][0]);
+                break;
             }
         }
-        if (idCheck == true) {
+
+        if (idCheck == true) { // 아이디가 존재하면 비밀번호 체크 회원번호를 이용해서 체크한다.
             if (password.equals(memberList[memberNumber][2])) {
                 pwCheck = true;
             }
@@ -45,12 +44,46 @@ public class MemberList {
                 System.out.println("===== 계시판 관리 =====");
                 System.out.println("1.사용자 관리");
                 System.out.print("메뉴 입력: ");
-                int page = sc.nextInt();
-                switch (page) {
+                int page = sc.nextInt(); // 메뉴 입력받음
+                switch (page) { // 입력받은 메뉴를 스위치 케이스문으로 구별 밑 각 메뉴들의 기능
                     case 1:
                         System.out.println("=== 사용자 관리 페이지 ===");
-                        System.out.println(Arrays.toString(memberList));
+                        for (int i = 0; i < memberList.length; i++) {  // 전체 사용자 출력 admin 포함
+                            for (int j = 0; j < 4; j++) {
+                                System.out.printf(memberList[i][j] + "\t");
+                            }
+                            System.out.println("");
+                        }
+                        System.out.println("=================");
+                        System.out.println("1.수정 2.제거 3.생성 4.이전"); //while 문으로 반복해줘야함
+                        System.out.print("메뉴 입력: ");
+                        int page1 = sc.nextInt();
+                        switch (page1) {
+                            case 1: //수정기능
+                                System.out.print("수정할 멤버의 회원번호를 입력하세요! > ");
+                                String modifyMId = sc.next();
+                                System.out.println("1.아이디 2.비밀번호 3.회원등급");
+                                System.out.println("=================");
+                                System.out.print("수정할 카테고리번호를 입력하세요 > ");
+                                int category = sc.nextInt();
+                                System.out.print("수정할 내용을 입력하세요 > ");
+                                memberList[Integer.parseInt(modifyMId)][category] = sc.next(); //입력 받은 카테고리값 변경
+
+                                for (int i = 0; i < 4; i++) {
+                                    System.out.printf(memberList[Integer.parseInt(modifyMId)][i] + "\t");
+                                }//나중 반복문을 통한 멤버리스트 전부를 보여주는것으로 만들기
+                                System.out.print("변경완료!\n");
+                                break; // 1번 브레이크
+                            case 2: //제거
+                                break;
+                            case 3: // 생성
+                                break;
+                            case 4: // 이전
+                                break;
+                        }
+
                         break;
+
                 }
             }
         } else {
