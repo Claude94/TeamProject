a03949b8860b5872ecf96805e266cd39e3c6da02
Claude94 <1claude1994@gmail.com>
2021-03-19 16:43:41 +0900

    [21.03.19] 글작성 검색 종료 완성~

--- src/TeamProject.java
+++ src/TeamProject.java
@@ -5,14 +5,15 @@ public class TeamProject {
     public static void main(String[] args) {
         //호면에 표시되는 기능
         System.out.println("계시판");
-        System.out.println("==================");
+        System.out.println("======================");
         System.out.println("5.계시글 작성");
         System.out.println("6.계시글 검색");
         System.out.println("7.프로그램 종료");
-        System.out.println("==================");
+        System.out.println("======================");
         System.out.print(">> ");
         //임시 배열
-        String[] newDoc = {"1", "안녕", "안녕하세요"};
+//        String[] newDoc = {"1", "안녕", "안녕하세요"};
+        String[] newDoc = {};
         //입력 받는 기능
         Scanner sc = new Scanner(System.in);
         int indexNum = sc.nextInt();
@@ -36,26 +37,28 @@ public class TeamProject {
                 temp[temp.length - 1] = newDocContent;
                 newDoc = temp;
                 temp = null;
-                // 배열 입력 확인 기능 (추후에 삭제 요망)
-                System.out.println(Arrays.toString(newDoc));
             }
+            // 작성 된 계시글 확인
+            System.out.println("\n==== 작성된 계시글 ====");
+            System.out.printf("번호 : %s \n제목 : %s \n내용 : %s", newDoc[0], newDoc[1], newDoc[2]);
         } else if (indexNum == 6) {
             // 작성글 검색 기능
             System.out.println("검색 할 글의 번호를 입력하세요");
             System.out.print(">> ");
             String indexSearchNum = sc.next();
             // 번호 매칭 기능
-            boolean searchContent = false;
+            boolean searchNum = false;
             int i = 0;
             for (i = 0; i < newDoc.length; i++) {
                 if (indexSearchNum.equals(newDoc[i])) {
-                    searchContent = true;
+                    searchNum = true;
                     break;
                 }
             }
             //검색 된 계시글 화면 표현
-            if (searchContent) {
-                System.out.println(Arrays.toString(newDoc));
+            if (searchNum) {
+                System.out.println("==== 검색 된 계시글 ====");
+                System.out.printf("번호 : %s \n제목 : %s \n내용 : %s", newDoc[0], newDoc[1], newDoc[2]);
             } else {
                 System.out.println("등록 된 번호가 없습니다");
                 System.out.print(">> ");
