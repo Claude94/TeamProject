7377cfc1ae69eb6d483572b7a133e320efccb699
felizju <felizju@gmail.com>
2021-03-21 17:30:12 +0900

    Board 수정 완료2

--- src/Board.java
+++ src/Board.java
@@ -7,8 +7,8 @@ public class Board {
         Scanner sc = new Scanner(System.in);
 
         String[][] boardArr = new String[1000][4];
+        int boardNum = 0; // 게시글 초기 번호
         int boardCount = 0; // 총 게시물 수
-
         while (true){
 
             // 프로그램 메뉴
@@ -33,9 +33,7 @@ public class Board {
                     System.out.println("# 게시판 글 등록을 시작합니다.");
 
                     String[] board = new String[4];
-
-                    System.out.print("- 글 번호 >> ");
-                    String boardNum = sc.nextLine();
+                    System.out.println("- 글 번호 >> "+ ++boardNum);
 
                     System.out.print("- 이름 >> ");
                     String userName = sc.nextLine();
@@ -46,7 +44,7 @@ public class Board {
                     System.out.print("- 글 내용 >> ");
                     String boardContent = sc.nextLine();
 
-                    board[0] = boardNum;
+                    board[0] = String.valueOf(boardNum);
                     board[1] = userName;
                     board[2] = boardTitle;
                     board[3] = boardContent;
@@ -54,7 +52,7 @@ public class Board {
                     boardArr[boardCount++] = board;
 
                     System.out.printf("%s님의 게시판 글이 정상 등록되었습니다.\n",userName);
-                    System.out.println(Arrays.toString(board));
+//                    System.out.println(Arrays.toString(board));
                     break;
 
                 case 2:
@@ -139,10 +137,12 @@ public class Board {
                                 String deleteMenu = sc.next();
                                 if(deleteMenu.equals("Y")){
                                     // 삭제 게시물 배열에서 삭제
-                                    for (int b = 0; b< boardArr.length-1; b++) {
+                                    for (int b = 0; b < boardArr.length-1; b++) {
                                         boardArr[b] = boardArr[b+1];
                                     }
+
                                     String[][] temp = new String[boardArr.length-1][];
+
                                     for (int j= 0; j < temp.length; j++) {
                                         temp[i] = boardArr[i];
                                     }
@@ -159,6 +159,7 @@ public class Board {
                                     for (int k = 0; k < boardCount; k++) {
                                         System.out.printf("글 번호 : %s | 작성자 : %s | 제목 : %s | 내용 : %s \n"
                                                 ,boardArr[k][0],boardArr[k][1],boardArr[k][2],boardArr[k][3]);
+
                                     }
 
                                 }else{
